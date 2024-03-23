8b91de5c0b296b05d2f8b94cf097731b4115bf84
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2024-03-24 07:26:08 +0900

    feat!: guaranee heap allocation using noncopyable generics

--- Sources/Box/Box.swift
+++ Sources/Box/Box.swift
@@ -1,75 +1,78 @@
 @propertyWrapper
-public struct Box<Value> {
-    private var object: BoxObject<Value>
+public struct Box<Value: ~Copyable>: ~Copyable {
+    @usableFromInline let pointer: UnsafeMutablePointer<Value>
 
+    @inlinable
     public var value: Value {
-        get { self.object.value }
-        set {
-            if !isKnownUniquelyReferenced(&self.object) {
-                self.object = self.object.copy()
-            }
-            self.object.value = newValue
-        }
+        _read { yield self.pointer.pointee }
+        _modify { yield &self.pointer.pointee }
     }
 
+    @inlinable
     public var wrappedValue: Value {
-        get { self.value }
-        set { self.value = newValue }
+        _read { yield self.value }
+        _modify { yield &self.value }
     }
 
-    public init(_ value: Value) {
-        self.object = .init(value)
+    public init(_ value: consuming Value) {
+        self.pointer = .allocate(capacity: 1)
+        self.pointer.initialize(to: value)
     }
 
-    public init(wrappedValue: Value) {
+    public init(wrappedValue: consuming Value) {
         self.init(wrappedValue)
     }
-}
 
-extension Box: TextOutputStreamable {
-    public func write<Target>(to target: inout Target) where Target: TextOutputStream {
-        print(self.value, terminator: "", to: &target)
+    deinit {
+        self.pointer.deinitialize(count: 1)
+        self.pointer.deallocate()
     }
 }
 
-extension Box: Equatable where Value: Equatable {
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.value == rhs.value
-    }
-}
-
-extension Box: Comparable where Value: Comparable {
-    public static func < (lhs: Self, rhs: Self) -> Bool {
-        lhs.value < rhs.value
-    }
-}
-
-extension Box: Hashable where Value: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(self.value)
-    }
-}
-
-extension Box: Error where Value: Error {}
-
-extension Box: Encodable where Value: Encodable {
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(self.value)
-    }
-}
-
-extension Box: Decodable where Value: Decodable {
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        self.init(try container.decode(Value.self))
-    }
-}
-
-extension Box: CustomStringConvertible where Value: CustomStringConvertible {
-    public var description: String { self.value.description }
-}
-
-extension Box: CustomDebugStringConvertible where Value: CustomDebugStringConvertible {
-    public var debugDescription: String { self.value.debugDescription }
-}
+// extension Box: TextOutputStreamable {
+//     public func write<Target>(to target: inout Target) where Target: TextOutputStream {
+//         print(self.value, terminator: "", to: &target)
+//     }
+// }
+//
+// extension Box: Equatable where Value: Equatable {
+//     public static func == (lhs: Self, rhs: Self) -> Bool {
+//         lhs.value == rhs.value
+//     }
+// }
+//
+// extension Box: Comparable where Value: Comparable {
+//     public static func < (lhs: Self, rhs: Self) -> Bool {
+//         lhs.value < rhs.value
+//     }
+// }
+//
+// extension Box: Hashable where Value: Hashable {
+//     public func hash(into hasher: inout Hasher) {
+//         hasher.combine(self.value)
+//     }
+// }
+//
+// extension Box: Error where Value: Error {}
+//
+// extension Box: Encodable where Value: Encodable {
+//     public func encode(to encoder: Encoder) throws {
+//         var container = encoder.singleValueContainer()
+//         try container.encode(self.value)
+//     }
+// }
+//
+// extension Box: Decodable where Value: Decodable {
+//     public init(from decoder: Decoder) throws {
+//         let container = try decoder.singleValueContainer()
+//         self.init(try container.decode(Value.self))
+//     }
+// }
+//
+// extension Box: CustomStringConvertible where Value: CustomStringConvertible {
+//     public var description: String { self.value.description }
+// }
+//
+// extension Box: CustomDebugStringConvertible where Value: CustomDebugStringConvertible {
+//     public var debugDescription: String { self.value.debugDescription }
+// }
